ce9e583ca5dcca1d4528d5c4489bf505492549cb
pepeunion89 <69573018+pepeunion89@users.noreply.github.com>
2023-10-11 13:16:50 -0300

    More design - Item product dialog

--- src/app/components/dialogs/dialogs.component.ts
+++ src/app/components/dialogs/dialogs.component.ts
@@ -1,6 +1,6 @@
 import { NgIf } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Products } from 'src/app/models/products';
 
 @Component({
@@ -10,7 +10,8 @@ import { Products } from 'src/app/models/products';
 })
 export class DialogsComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Products) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Products,
+                      public dialogRef: MatDialogRef<DialogsComponent>) {}
 
   dataForm: any={
 
@@ -21,6 +22,69 @@ export class DialogsComponent {
   }
 
   paymentMethodSelected:string = "Transferencia";
+  flagSeeDetails: number = 0;
+
+  closeWindow(){
+    this.dialogRef.close();
+  }
+
+  seeDetails(){
+
+    var detailsContainer = document.getElementsByClassName('description')[0] as HTMLElement;
+    var productImg = document.getElementsByClassName('product-img')[0] as HTMLElement;
+    var productTitle = document.getElementsByClassName('product-title')[0] as HTMLElement;
+    var containerImg = document.getElementsByClassName('container-img')[0] as HTMLElement;
+    var containerImgTitle = document.getElementsByClassName('container-img-title')[0] as HTMLElement;
+    var arrow = document.getElementsByClassName('arrow')[0] as HTMLElement;
+    var seeDetailsButton = document.getElementsByClassName('see-details')[0] as HTMLElement;
+    
+    if(this.flagSeeDetails===0){
+
+      detailsContainer.style.height = '220px';
+      //detailsContainer.style.marginTop = '-260px';
+
+      containerImg.style.width = '80px';
+      containerImg.style.height = '80px';
+
+      productImg.style.height = '100%';
+
+      productTitle.style.fontSize = '20px';
+      productTitle.style.padding = '16px 0 0 0';
+
+      containerImgTitle.style.display = 'flex';
+
+      arrow.style.rotate = "270deg";
+
+      //seeDetailsButton.style.textDecoration = 'underline';
+
+      this.flagSeeDetails=1;
+
+    }else{
+
+      detailsContainer.style.height = '0px';
+      //detailsContainer.style.marginTop = '0px';
+
+      productImg.style.height = '220px';
+
+      containerImg.style.height = '220px';
+      containerImg.style.width = 'auto';
+
+      productTitle.style.fontSize = '26px';
+      //productTitle.style.lineHeight = '26px';
+
+      productImg.style.height = 'auto';
+
+      containerImgTitle.style.display = 'block';
+
+      arrow.style.rotate = "90deg";
+
+      //seeDetailsButton.style.textDecoration = 'none';
+
+      this.flagSeeDetails=0;
+
+    }
+
+  }
 
   goToPayment(){
     var firstContainer = document.getElementsByClassName('first-container')[0] as HTMLElement;

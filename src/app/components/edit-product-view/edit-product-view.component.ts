22f4fd67b9c9058c0fb56b173815bda06337a568
pepeunion89 <69573018+pepeunion89@users.noreply.github.com>
2024-01-05 12:39:40 -0300

    Edit product view developing

--- src/app/components/edit-product-view/edit-product-view.component.ts
+++ src/app/components/edit-product-view/edit-product-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ProductsApi } from 'src/app/models/product-api';
 
@@ -7,30 +7,80 @@ import { ProductsApi } from 'src/app/models/product-api';
   templateUrl: './edit-product-view.component.html',
   styleUrls: ['./edit-product-view.component.scss']
 })
-export class EditProductViewComponent {
+export class EditProductViewComponent implements OnInit{
 
   productToEdit: ProductsApi;
-  colorNumbers = [1,2,3,4,5];
+  colorNumbers: number[]; 
+  colorNames: string[];
+  colorHex: string[];
+  colorsChecked: boolean[] = [false,false,false,false,false];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: ProductsApi,
               private dialogRef: MatDialogRef<EditProductViewComponent>){
 
     this.productToEdit = data;
+
+    this.colorNumbers = [1,2,3,5,4];
+
+    this.colorNames = [this.productToEdit.color1_name,
+                       this.productToEdit.color2_name,
+                       this.productToEdit.color3_name,
+                       this.productToEdit.color4_name,
+                       this.productToEdit.color5_name];
+
+    this.colorHex = [this.productToEdit.color1_hex,
+                     this.productToEdit.color2_hex,
+                     this.productToEdit.color3_hex,
+                     this.productToEdit.color4_hex,
+                     this.productToEdit.color5_hex];
+
+    if(this.productToEdit.color1_hex != null){
+      this.colorsChecked[0] = true;
+    }
+
+    if(this.productToEdit.color2_hex != null){
+      this.colorsChecked[1] = true;
+    }
+
+    if(this.productToEdit.color3_hex != null){
+      this.colorsChecked[2] = true;
+    }
+
+    if(this.productToEdit.color4_hex != null){
+      this.colorsChecked[3] = true;
+    }
+
+    if(this.productToEdit.color5_hex != null){
+      this.colorsChecked[4] = true;
+    }
     
   }
 
   ngOnInit(){
 
+    let count = 1;
 
+      for(let colorChecked of this.colorsChecked){
 
+        if(colorChecked===true){
+          console.log(count);
+          this.eodColor(count);
+        }
+
+        count+=1;
+
+      }
+
+    
   }
+
 
   eodColor(number: number){
 
     let colorTextInput = document.getElementsByClassName('colorName-'+number)[0] as HTMLInputElement;
     let colorInput = document.getElementsByClassName('colorHex-'+number)[0] as HTMLInputElement;
     let chkboxInput = document.getElementsByClassName('enableColor-'+number)[0] as HTMLInputElement;
-
+    
     if(chkboxInput.checked){
       colorInput.removeAttribute('disabled');
       colorInput.style.backgroundColor='lightgreen';
